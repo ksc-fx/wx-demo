5af0c9f95515ee9c2d2b5ff3f7c699efdcc3bb29
huanghaojie <huanghaojie@kingsoft.com>
2018-04-16 17:39:15 +0800

    Update： 首页 下拉加载

---
+++ client/pages/component/icon/index.js
@@ -0,0 +1,62 @@
+Page({
+  data: {
+    iconType: [
+      "success",
+      "success_no_circle",
+      "info",
+      "warn",
+      "waiting",
+      "cancel",
+      "download",
+      "search",
+      "clear"
+    ],
+    iconTypes: [
+      {
+        type: "success",
+        title: "成功",
+        text: "用于成功"
+      },
+      {
+        type: "success_no_circle",
+        title: "没圈的成功",
+        text: "用于成功11"
+      },
+      {
+        type: "info",
+        title: "提示",
+        text: "用于成功222"
+      },
+      {
+        type: "warn",
+        title: "警告",
+        text: "用于成功"
+      },
+      {
+        type: "等待",
+        title: "成功",
+        text: "用于成功"
+      },
+      {
+        type: "cancel",
+        title: "取消",
+        text: "用于成功"
+      },
+      {
+        type: "download",
+        title: "下载",
+        text: "用于成功"
+      },
+      {
+        type: "search",
+        title: "搜索",
+        text: "用于成功"
+      },
+      {
+        type: "clear",
+        title: "取消2",
+        text: "用于成功"
+      }
+    ]
+  }
+});
--- client/pages/component/index/index.js
+++ client/pages/component/index/index.js
@@ -1,116 +1,116 @@
 // pages/component/index.js
 Page({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        list: [
+            {
+                id: "view",
+                name: "视图容器",
+                open: false,
+                pages: ["view", "scroll-view", "swiper"]
+            },
+            {
+                id: "content",
+                name: "基础内容",
+                open: false,
+                pages: ["text", "icon", "progress"]
+            },
+            {
+                id: "form",
+                name: "表单组件",
+                open: false,
+                pages: [
+                    "button",
+                    "checkbox",
+                    "form",
+                    "input",
+                    "label",
+                    "picker",
+                    "radio",
+                    "slider",
+                    "switch",
+                    "textarea"
+                ]
+            },
+            {
+                id: "nav",
+                name: "导航",
+                open: false,
+                pages: ["navigator"]
+            },
+            {
+                id: "media",
+                name: "媒体组件",
+                open: false,
+                pages: ["image", "audio", "video"]
+            },
+            {
+                id: "map",
+                name: "地图",
+                pages: ["map"]
+            },
+            {
+                id: "canvas",
+                name: "画布",
+                pages: ["canvas"]
+            }
+        ]
+    },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    list: [
-      {
-        id: 'view',
-        name: '视图容器',
-        open: false,
-        pages: ['view', 'scroll-view', 'swiper']
-      }, {
-        id: 'content',
-        name: '基础内容',
-        open: false,
-        pages: ['text', 'icon', 'progress']
-      }, {
-        id: 'form',
-        name: '表单组件',
-        open: false,
-        pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'radio', 'slider', 'switch', 'textarea']
-      }, {
-        id: 'nav',
-        name: '导航',
-        open: false,
-        pages: ['navigator']
-      }, {
-        id: 'media',
-        name: '媒体组件',
-        open: false,
-        pages: ['image', 'audio', 'video']
-      }, {
-        id: 'map',
-        name: '地图',
-        pages: ['map']
-      }, {
-        id: 'canvas',
-        name: '画布',
-        pages: ['canvas']
-      }
-    ]
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {},
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {},
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {},
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {},
 
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {},
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-  kindToggle: function (e) {
-    console.log(e);
-    const id = e.currentTarget.id;
-    const list = this.data.list;
-    for (var i = 0, len = list.length; i < len; ++i) {
-      if (list[i].id == id) {
-        list[i].open = !list[i].open
-      } else {
-        list[i].open = false
-      }
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {},
+    kindToggle: function(e) {
+        console.log(e);
+        const id = e.currentTarget.id;
+        const list = this.data.list;
+        for (var i = 0, len = list.length; i < len; ++i) {
+            if (list[i].id == id) {
+                list[i].open = !list[i].open;
+            } else {
+                list[i].open = false;
+            }
+        }
+        this.setData({
+            list: list
+        });
     }
-    this.setData({
-      list: list
-    });
-  }
-})
+});
